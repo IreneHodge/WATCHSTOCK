0d6b74a73ed075c77aeeb7ddc02a278f243a68c7
Irene Hodge <irenehodge90@gmail.com>
2021-08-03 12:20:36 -0600

    updated routes.ts

--- src/controllers/routes/routes.ts
+++ src/controllers/routes/routes.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get((request: Request, response: Response) =>
-    response.send("Server Working").status(200).end()
-  );
+router.route("/");
+// .get((request: Request, response: Response) =>
+//   response.send("Server Working").status(200).end()
+// );
 
 module.exports = router;
