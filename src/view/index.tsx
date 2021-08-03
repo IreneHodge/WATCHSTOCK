830e000161a0cba4f47bc71a80065d3da7d97b17
Irene Hodge <irenehodge90@gmail.com>
2021-08-02 19:50:14 -0600

    created basic index react/typescript component

---
+++ src/view/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <App title="Stock-A-Roo" subtitle="Your Favorite Stock App" />,
+  document.getElementById("root")
+);
