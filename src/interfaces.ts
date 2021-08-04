a7ce2f8d57ea0433277f6ff0b166a875a1770dbc
Irene Hodge <irenehodge90@gmail.com>
2021-08-04 12:56:08 -0600

    created interfaces file for easy export

---
+++ src/interfaces.ts
@@ -0,0 +1,14 @@
+export interface CompanyData {
+  logo: String;
+  ceo: String;
+  url: String;
+  description: String;
+  exchange: String;
+  name: String;
+  symbol: String;
+  similar: String[];
+  open: Number;
+  high: Number;
+  low: Number;
+  close: Number;
+}
