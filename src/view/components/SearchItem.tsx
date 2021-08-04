c5e2c3751b211c2f82c9f80f337cd40f4a7ad053
Irene Hodge <irenehodge90@gmail.com>
2021-08-04 12:59:57 -0600

    updated SearchItem to dynamically render searched company data

--- src/view/components/SearchItem.tsx
+++ src/view/components/SearchItem.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { FC } from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import AddToWatchList from "./AddToWatchList";
+import { CompanyData } from "../../interfaces";
+
+interface SearchItemProps {
+  companyData: CompanyData;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,29 +22,36 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function CenteredGrid() {
+const SearchItem: FC<SearchItemProps> = ({ companyData }) => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>company logo</Paper>
-          <Paper className={classes.paper}>Company Name</Paper>
-          <Paper className={classes.paper}>Market & Symbol</Paper>
+          <Paper className={classes.paper}>
+            <img src={companyData.logo} alt="Company Logo" />
+          </Paper>
+          <Paper className={classes.paper}>{companyData.name}</Paper>
+          <Paper className={classes.paper}>
+            {companyData.exchange}: {companyData.symbol}
+          </Paper>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>About:</Paper>
-          <Paper className={classes.paper}>Company Description</Paper>
+          <Paper className={classes.paper}>About</Paper>
+          <Paper className={classes.paper}>{companyData.url}</Paper>
+          <Paper className={classes.paper}>{companyData.description}</Paper>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>Open</Paper>
-          <Paper className={classes.paper}>High</Paper>
-          <Paper className={classes.paper}>Low</Paper>
-          <Paper className={classes.paper}>Close</Paper>
+          <Paper className={classes.paper}>Open: {companyData.open}</Paper>
+          <Paper className={classes.paper}>High: {companyData.high}</Paper>
+          <Paper className={classes.paper}>Low: {companyData.low}</Paper>
+          <Paper className={classes.paper}>Close: {companyData.close}</Paper>
           <AddToWatchList />
         </Grid>
       </Grid>
     </div>
   );
-}
+};
+
+export default SearchItem;
