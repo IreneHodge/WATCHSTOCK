7efbfa07c856c858a28243829ba7746327e72251
Irene Hodge <irenehodge90@gmail.com>
2021-08-04 11:58:13 -0600

    added search and watchlist routes

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,16 +1,22 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./routes/routes");
+const polygonRoutes = require("./routes/polygonRoutes");
+const dbRoutes = require("./routes/dbRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, "..", "../build")));
 
-app.use("/", routes);
+app.use("/search", polygonRoutes);
+
+app.use("/savedCompanies", dbRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  //handle root
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost${PORT}`);
